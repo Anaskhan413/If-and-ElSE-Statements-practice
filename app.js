0547169440e7ef7b0f745743f18925b3adfc31a1
Anas khan <anaskhan41347@gmail.com>
2023-02-12 23:44:53 +0500

    If and ElSE Statements practice

---
+++ app.js
@@ -0,0 +1,105 @@
+
+
+
+
+// user agae equal to 20
+
+
+
+
+var age = 20;
+if(age = 10){
+    console.log('ELEGIBLE')
+}else{
+        console.log("NOT ELEGIBLE")
+}
+
+// user GENDER equal to MALE
+
+var Gender =( "Male");
+
+
+if( Gender = ("male") ){
+    console.log("USER ALLOW")
+}else{
+    console.log("USER NOT ALLOW")
+}
+
+// user GENDER equal to MALE
+
+
+
+
+
+var Userage = 20;
+if(Userage === 20){
+    console.log(typeof 20)
+}
+
+
+
+
+
+// user age Greater than 20
+
+var age = 22;
+if(age > 20){
+    console.log('Yes age is Greater than 20')
+}else{
+        console.log(" Age is Not Greater than 20")
+}
+
+
+
+
+// user age Greater than and equal to 20
+
+var age = 20;
+if(age >= 20){
+    console.log('Yes age is Greater than 20 and Equal to 20')
+}else{
+        console.log(" Age is Not Greater than 20")
+}
+
+
+
+
+
+// user age Greater than and equal to 18 && userAge =<40
+
+var age = 18;
+if(age >= 18 && age <= 40 ){
+    console.log(' to using &&')
+}else{
+        console.log(" yes it is not  ")
+}
+
+
+
+
+
+// user age Greater than and equal to 18 && userAge =<40
+var age = 18;
+if(age >= 18 || age <= 40 ){
+    console.log(' to using ||')
+}else{
+        console.log(" yes it is not  ")
+}
+
+
+
+
+
+
+// user age Greater than and equal to 18 && userAge =<40
+
+var age = 18;
+var Gender = ("Male");
+
+
+if( age>=18 && ( Gender == "Male"|| Gender == "Female")     ){
+console.log(" Condition is true")
+}else{
+    console.log(" Condition is not true")
+
+}
